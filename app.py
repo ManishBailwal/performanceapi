cc7443af201ce7bf1bc63f0e827ddff265f3924c
ManishBailwal <102870593+ManishBailwal@users.noreply.github.com>
2025-06-06 08:10:41 +0530

    changes req

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 import pandas as pd
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
+CORS(app, origins=["http://localhost:3000", "hhttps://www.mudbloom.com/"])
 
 # Load player stats CSV once
 df = pd.read_csv("generated_player_stats.csv")
